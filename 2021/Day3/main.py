c01fceb534f49829a45cc82de34aeb98be4b26c8
CernohorskyVaclav <vaclav.cernohorsky@gmail.com>
2021-12-06 17:05:05 +0100

    Day 3

--- 2021/Day3/main.py
+++ 2021/Day3/main.py
@@ -1,3 +1,4 @@
+import operator
 
 def main():
     inp = get_input()
@@ -6,7 +7,6 @@ def main():
 
 
 def part_one(inp):
-    inp = inp.split("\n")
     one = 0
     zero = 0
     gamma = str()  # most common
@@ -34,16 +34,43 @@ def part_one(inp):
 
 
 def part_two(inp):
-    listo = [len(inp[0])]
+    ogr = get_ogr_ang_csr(inp, operator.gt, "0")  # oxygen generator rating
+    scr = get_ogr_ang_csr(inp, operator.lt, "1")  # CO2 scrubber rating
+
+    return int(ogr, 2) * int(scr, 2)
+
+
+def get_ogr_ang_csr(inp, operation, if_equal):
+    one = 0
+    zero = 0
     for x in range(len(inp[0])):
         for y in range(len(inp)):
-            listo[x].append(inp[y][x])
-    return listo
+            if inp[y][x] == "1":
+                one += 1
+            elif inp[y][x] == "0":
+                zero += 1
+            else:
+                print("error, wrong input (neither 0 or 1)")
+        if operation(one, zero):                 # --v-- this is what you want to be removed
+            inp = list(filter(lambda val: val[x] != "0", inp))
+            one = 0
+            zero = 0
+        elif operation(zero, one):               # --v-- this is what you want to be removed
+            inp = list(filter(lambda val: val[x] != "1", inp))
+            one = 0
+            zero = 0
+        else:
+            inp = list(filter(lambda val: val[x] != if_equal, inp))
+            one = 0
+            zero = 0
+
+        if len(inp) == 1:
+            return inp[0]
 
 
 def get_input():
     f = open("input.txt", "r")
-    inp = f.read()
+    inp = f.read().split("\n")
 
     return inp
 
